eae26f07894b12467160ff417c21593734b72459
Geovani29 <pgeovani@uninorte.edu.co>
2022-11-16 21:28:31 +0000

    commit

--- code/estacionamiento.py
+++ code/estacionamiento.py
@@ -26,3 +26,36 @@ class Estacionamiento(Espacios):
                 else:
                     print("Valor incorrecto, ingrese otro.")
                     self.cobrar()
+    def menu(self) -> None:
+        self.iniciar()
+        self.cobrar()
+
+    def min_hor_dia(self) -> None:
+        print("Necesita parquear por:")
+        print("1-Minutos")
+        print("2-Horas")
+        print("3-Dias")
+        option = int(input())
+        if (option == 1):
+            print("¿Cúantos minutos?")
+            minutos = int(input())
+            self.money = self.money*minutos
+            self.pagaras()
+        else:
+            if (option == 2):
+                print("¿Cúantas horas?")
+                horas = int(input())
+                self.money = self.money*horas*60
+                self.pagaras()
+            else:
+                if (option == 3):
+                    print("¿Cúantos dias?")
+                    dias = int(input())
+                    self.money = self.money*dias*60*24
+                    self.pagaras()
+                else:
+                    print("Incorrecto, ingrese otro valor")
+                    self.min_hor_dia()
+
+    def pagaras(self):
+        print(f"Debe pagar {self.money}")                    
